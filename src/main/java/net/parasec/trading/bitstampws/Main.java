fe1a6a9c077e8ab3a85b5b045ed8532869f7ede1
phil8192 <phil@parasec.net>
2019-05-21 06:05:09 +0000

    start client interface

--- src/main/java/net/parasec/trading/bitstampws/Main.java
+++ src/main/java/net/parasec/trading/bitstampws/Main.java
@@ -2,6 +2,7 @@ package net.parasec.trading.bitstampws;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Client client = new Client();
+		Client client = new BitstampClient();
+		client.subscribe("live_orders", "btc_usd");
 	}
 }
