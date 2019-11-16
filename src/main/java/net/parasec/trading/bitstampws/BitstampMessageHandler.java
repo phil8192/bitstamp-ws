e748f7696ea7feb8a0b3533c05605bddb2313d25
phil8192 <phil@parasec.net>
2019-11-16 19:47:01 +0000

    doc bitstamp message handler

--- src/main/java/net/parasec/trading/bitstampws/BitstampMessageHandler.java
+++ src/main/java/net/parasec/trading/bitstampws/BitstampMessageHandler.java
@@ -3,9 +3,30 @@ package net.parasec.trading.bitstampws;
 import net.parasec.trading.bitstampws.websocket.Event;
 
 public interface BitstampMessageHandler<T extends Event> {
-
-	// implementer must return immediately.
-	// it is assumed that this will go on some queue.
+	/**
+	 * Message handler for various Bitstamp message types.
+	 *
+	 * Implementer must return immediately.
+	 * It is assumed that the message is placed on a queue, e.g., @see java.util.concurrent.ConcurrentLinkedQueue
+	 *
+	 * Examples:
+	 *
+	 * BitstampMessageHandler<OrderEvent> orderHandler = order
+	 *   -> System.out.println(order);
+	 *
+	 * BitstampMessageHandler<TradeEvent> tradeHandler = trade
+	 *   -> System.out.println(trade);
+	 *
+	 * BitstampMessageHandler<OrderBookEvent> orderBookHandler = orderBookEvent
+	 * 	 -> System.out.println(orderBookEvent);
+	 *
+	 * BitstampMessageHandler<DetailOrderBookEvent> detailOrderBookHandler = detailOrderBookEvent
+	 *   -> System.out.println(detailOrderBookEvent);
+	 *
+	 * BitstampMessageHandler<DiffOrderBookEvent> diffOrderBookHandler = diffOrderBookEvent
+	 *   -> System.out.println(diffOrderBookEvent);
+	 *
+	 * @param message @see net.parasec.trading.bitstampws.websocket.Event
+	 */
 	void onMessage(T message);
-
 }
